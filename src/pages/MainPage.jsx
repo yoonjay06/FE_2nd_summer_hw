afc36d259bc5f3386ba43f8ec43915149f76880d
yoonjay06 <yjaewon1020@gmail.com>
2026-07-17 19:51:47 +0900

    grid 적용

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,14 +1,25 @@
 import dummyData from '../data/dummy_data';
 import PostCard from '../components/PostCard';
+import styled from 'styled-components';
 
 function MainPage() {
     return (
-        <div>
+        <GridWrapper>
             {dummyData.map((post) => (
                 <PostCard key={post.id} post={post} />
             ))}
-        </div>
+        </GridWrapper>
     );
 }
+
+const GridWrapper = styled.div`
+    max-width: 1080px;
+    min-width: 0;
+    margin: 0 auto;
+    padding: 40px 20px;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 32px;
+`
 
 export default MainPage;
